23432a955b2202b231ed3500ee057236b49e17dc
DiegoAladren <146423034+DiegoAladren@users.noreply.github.com>
2024-03-21 18:57:23 +0100

    Create Archivo3.java
    Clase creada con sus primeras variables.

---
+++ Archivo3.java
@@ -0,0 +1,11 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+}
