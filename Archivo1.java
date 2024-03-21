85f55116fe538fa906c698388a269cb941d1f56a
DiegoAladren <146423034+DiegoAladren@users.noreply.github.com>
2024-03-21 18:50:40 +0100

    Create Archivo1.java
    En este primer commit creo el archivo con su primer metodo.

---
+++ Archivo1.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+    public static void main(String[] args) {
+
+    double numero1;
+    double numero2;
+
+   
+    public Calculadora(double a, double b) {
+        this.numero1 = a;
+        this.numero2 = b;
+    }
+
+    public double suma() {
+        return this.numero1 + this.numero2;
+    }
+  
+  }
+}
