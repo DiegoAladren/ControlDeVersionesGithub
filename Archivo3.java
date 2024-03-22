3fa675ad47e20be4d8f2dd7d173f0b5abfe1b293
DiegoAladren <146423034+DiegoAladren@users.noreply.github.com>
2024-03-22 12:21:43 +0100

    numero tel Archivo3.java
    Añadida variable número telefono

--- Archivo3.java
+++ Archivo3.java
@@ -2,10 +2,13 @@ public class Persona {
     private String nombre;
     private int edad;
     private String dni;
+    private String numeroTelefono;
 
     public Persona(String nombre, int edad, String dni) {
         this.nombre = nombre;
         this.edad = edad;
         this.dni = dni;
     }
+
+    
 }
